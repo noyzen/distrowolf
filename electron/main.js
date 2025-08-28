28fa0ab281cc25e09d5e39d7579a1fe31c4b00ca
Behzad Daneshpour (Noyzen) <noyzen@gmail.com>
2025-08-28 23:38:22 +0000

    i dont want you to build desktop app or appimage for me, i want you to s

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const loadUrl = isDev
+    ? 'http://localhost:9002'
+    : `file://${path.join(__dirname, '../out/index.html')}`;
+    
+  win.loadURL(loadUrl);
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
