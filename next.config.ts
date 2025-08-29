81f350da84f89951b2cb4d6e6ee30f77e5a750a2
Behzad Daneshpour (Noyzen) <noyzen@gmail.com>
2025-08-29 21:29:27 +0000

    read thsi guide and see if you can solve appimage white page problems an

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 
 import type {NextConfig} from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'out',
-  assetPrefix: './',
+  assetPrefix: isProd ? './' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
